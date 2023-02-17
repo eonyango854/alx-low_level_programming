9ca93ce965ef87f36ae63454a6a7b5df979f38c7
eonyango854 <eonyango854@gmail.com>
2023-02-16 17:07:01 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * program that prints with proper grammer
+ * return - 0 (success)
+ */
+int main (void)	
+{  
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
